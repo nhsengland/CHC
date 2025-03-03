29702e46547e8b62f959c07a8742dd02195f0368
rwatson-nhs <rebecca.watson54@nhs.net>
2025-03-03 11:34:30 +0000

    Added summary

--- PLDS Care Package Dashboard/2 - Care Package View Financial Year (YTD).sql
+++ PLDS Care Package Dashboard/2 - Care Package View Financial Year (YTD).sql
@@ -1,3 +1,9 @@
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+-- View selects the latest care package record per Financial Year from the Care Package view. This can be used to count the number of open care
+-- packages per Financial Year.
+-- View feeds the tabs of the Care Package dashboard that are prefixed with 'FY'. 
+--------------------------------------------------------------------------------------------------------------------------------------------------------	
+
 SELECT  a.*
 FROM (
 		SELECT ROW_NUMBER() OVER(PARTITION BY CarePackagePatientID, FinancialYear	ORDER BY  ReportingDate DESC) AS RN
